f781c22975de1f4f79edf043bc3cd18e95ac831d
guillem <guillem.cordoba@gmail.com>
2016-11-03 14:14:36 +0100

    First version of the structure of the app implemented
    RLOCs menu detail implemented, switch views implemented

--- app/controllers/navigationController.js
+++ app/controllers/navigationController.js
@@ -5,19 +5,23 @@
  		$mdSidenav('right').open();
  	};
  	$scope.closeSideMenu = function() {
- 		$mdSidenav('right').close();
+ 		$mdSidenav('right')
+ 			.close()
+ 			.then(function(){
+ 				$scope.detailMenuState = "";
+ 			});
  	};
  	
  	$scope.showEidDetails = function($eid) {
  		$scope.openSideMenu();
  		// CALL LISP MODULE
  		$scope.eid = $eid;
-	 	$scope.state = "eid";
+	 	$scope.detailMenuState = "eid";
  	};
  	$scope.showRlocDetails = function($rloc) {
  		$scope.openSideMenu();
  		$scope.rloc = $rloc;
- 	 	$scope.state = "rloc";
+ 	 	$scope.detailMenuState = "rloc";
  	};
 
 	eid = {
@@ -53,6 +57,41 @@
 			}
 		]
 	};
-	$timeout(function () { $scope.showEidDetails(eid); }, false);
+
+	rloc = {
+		adress: "1.1.1.1",
+		priority: 2,
+		weight: 10,
+		eids: [
+			{
+				adress: "11.11.11.11"
+			},
+			{
+				adress: "11.11.11.11"
+			},
+			{
+				adress: "11.11.11.11"
+			},
+			{
+				adress: "11.11.11.11"
+			},
+			{
+				adress: "11.11.11.11"
+			},
+			{
+				adress: "11.11.11.11"
+			},
+			{
+				adress: "11.11.11.12"
+			},
+			{
+				adress: "11.11.11.13"
+			},
+			{
+				adress: "11.11.11.14"
+			}
+		]
+	};
+	$timeout(function () { $scope.showRlocDetails(rloc); }, false);
 
  }])
